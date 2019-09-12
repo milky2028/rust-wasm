ebc779e4d9052ec2051bf3e4876aecdd401b65fb
Tyler <tgross@skyroad.com>
2019-09-12 16:35:40 -0500

    you tryna get the pipe markup language

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,88 @@
-use colored::*;
-use rand::random;
-use std::io;
-// use serde_json::{Result, Value};
-// use wasm_bindgen::prelude::*;
+// use serde::{Deserialize, Serialize};
+// use serde_json::{Result, Error};
 
-// To create functions suitable for web assembly:
-// Add these to cargo.toml
-// [lib]
-// crate-type = ["cdylib", "rlib"]
-
-// some demos for deserializing data to a strictly-typed structure
-// struct JsonData {
-//   userId: u8,
-//   id: u8,
-//   title: String,
-//   completed: bool
+// #[derive(Serialize, Deserialize)]
+// struct Tyler {
+//     first_name: String,
+//     last_name: String
 // }
 
-// Add this above and function you want to make available to wasm_bindgen
-// #[wasm_bindgen]
-// pub fn weighted_average(json: &str) -> JsonData {
-//   let val: JsonData = serde_json::from_str(json);
-//   val
+// const JSON: &str = r#"
+//   {
+//     "first_name": "Tyler",
+//     "last_name": "Gross"
+//   }
+// "#;
+
+// fn deserialze(json: &str) -> Result<Tyler, Error> {
+//     let deserialzed_json = serde_json::from_str(json);
+//     deserialzed_json
 // }
 
 // fn main() {
-//   println!(
-//     "{}",
-//     weighted_average("{\"userId\":1,\"id\":1,\"title\":\"delectus aut autem\",\"completed\":false}")
-//   );
+//     deserialze(JSON);
 // }
 
-fn user_guess() -> u8 {
-    loop {
-        println!("{}\n", "Make a guess:".bold().blue());
-        let mut user_guess = String::new();
-        io::stdin()
-            .read_line(&mut user_guess)
-            .expect("Could not read input");
-        user_guess
-            .trim()
-            .parse::<u8>()
-            .expect("Did you enter a number?");
+// #[derive(PartialEq)]
+// enum SpiderMen {
+//     Peter,
+//     Miles
+// }
+
+// struct Thing {
+//     pep: String,
+//     people: Vec<String>,
+// }
+
+// fn main() {
+//     let x = Thing {
+//         pep: String::from("Tyler"),
+//         people: vec![String::from("Dad"), String::from("Mom")],
+//     };
+//     println!("\n I am {} \n {} \n {} \n", x.pep, x.people[0], x.people[1]);
+//     println!("\n First spider is {}", SpiderMen::Peter);
+// }
+
+use colored::*;
+use std::io::stdin;
+
+// Convert some of *this* text to a bunch of |dong| emojies
+
+enum State {
+    Normal,
+    Fire,
+    Eggplant,
+}
+
+fn parse_character(state: State, character: char) -> (State, Option<char>) {
+    use self::State::*;
+    match (state, character) {
+        (Normal, '|') => (Eggplant, None),
+        (Normal, '*') => (Fire, None),
+        (Normal, other) => (Normal, Some(other)),
+        (Fire, '*') => (Normal, None),
+        (Fire, _) => (Fire, Some('🔥')),
+        (Eggplant, '|') => (Normal, None),
+        (Eggplant, _) => (Eggplant, Some('🍆')),
     }
-
-    // let mut guess_display = String::new();
-    // if user_guess_as_number == correct_answer {
-    //     guess_display = user_guess.green().to_string();
-    // } else {
-    //     guess_display = user_guess.red().to_string();
-    // }
-
-    // println!("{} {}", "You guessed:", guess_display)
 }
 
 fn main() {
-    println!("\n\n\n{}", "This is a guessing game!".bold().purple());
-    let correct_answer = random::<u8>();
-    println!(
-        "\n\n{} {}",
-        "The correct answer is:".bold(),
-        correct_answer.to_string().green().bold()
-    );
-    let a = user_guess();
+    println!("\n\n{}", "Enter some text to markup:".blue().bold());
+
+    let mut input = String::new();
+    stdin()
+        .read_line(&mut input)
+        .expect("Somehow, there wasn't anything entered");
+    let mut state = State::Normal;
+    let mut output = String::new();
+    for character in input.chars() {
+        let (new_state, character_output) = parse_character(state, character);
+        if let Some(c) = character_output {
+            output.push(c);
+        }
+
+        state = new_state;
+    }
+    println!("{}", output);
 }
